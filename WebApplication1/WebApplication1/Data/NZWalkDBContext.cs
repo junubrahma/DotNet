db0113da62f6aa7601a73f1d2619a2af5ec0af50
junubrahma <junubrahma@JUNUBRAHMA01>
2022-10-23 12:37:28 +0530

    Commit no 1

---
+++ WebApplication1/WebApplication1/Data/NZWalkDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Domain;
+
+namespace WebApplication1.Data
+{
+    public class NZWalkDBContext :DbContext
+    {
+        public NZWalkDBContext(DbContextOptions<NZWalkDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks{ get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty{ get; set; }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
